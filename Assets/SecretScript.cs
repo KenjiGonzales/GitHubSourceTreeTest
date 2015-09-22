a5f5c4b7f4b7d2628821f49df6925e5e7e938bd8
KenjiGonzales <kenny_g3@hotmail.co.uk>
2015-09-22 10:53:27 +0100

    Added Scripts

---
+++ Assets/SecretScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecretScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("You have found the Golden Spoon, now go forth and devour the Ancient Soup");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
