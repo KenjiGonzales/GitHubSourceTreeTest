6e0cfca13164736efd2109e747ff36d398899509
KenjiGonzales <kenny_g3@hotmail.co.uk>
2015-09-22 10:58:08 +0100

    Added something else to script

--- Assets/SecretScript.cs
+++ Assets/SecretScript.cs
@@ -7,6 +7,7 @@ public class SecretScript : MonoBehaviour {
 	void Start () {
 		print ("You have found the Golden Spoon, now go forth and devour the Ancient Soup");
 		print ("Something has changed");
+		print ("Added something else");
 	}
 	
 	// Update is called once per frame
